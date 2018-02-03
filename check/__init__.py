37867f055a0596e310b6958f0c6b3992bef42df7
Markus Witt <feuerrot@feuerrot.org>
2018-02-03 20:44:58 +0100

    change stuff

--- check/__init__.py
+++ check/__init__.py
@@ -15,27 +15,41 @@ class Check:
 				" ".join([str(arg) for arg in args])
 			)
 	
+	def __filter_mx(self, mx):
+		return mx
+
 	def _query(self, type, hostname=None):
+		if type == dns.rdatatype.MX:
+			hostname = self.__filter_mx(hostname or self.hostname)
 		return dns.resolver.query(hostname or self.hostname, type)
 	
 	def _aaaa(self, hostname=None):
-		query = self._query('AAAA', hostname)
-		
-		return [answer.to_text() for answer in query.rrset]
+		query = self._query(dns.rdatatype.AAAA, hostname)
+		return [answer for answer in query.rrset]
 	
 	def _ns(self, hostname=None):
-		query = self._query('NS')
+		query = self._query(dns.rdatatype.NS)
 		ns_set = {}
 		for ns in query.rrset:
 			try:
-				ns_set[ns.to_text()] = self._aaaa(ns.to_text())
+				ns_set[ns] = self._aaaa(ns.to_text())
 			except dns.resolver.NoAnswer:
 				self._debug("nameserver", ns, "has no AAAA record", level="!")
-				#ns_set[ns] = None
-		print(ns_set)
+
+		return ns_set
 	
 	def _mx(self):
-		pass
+		query = self._query(dns.rdatatype.MX)
+		mx_set = {}
+		for mx in query.rrset:
+			try:
+				mx_set[mx] = self._aaaa(mx.exchange.to_text())
+			except dns.resolver.NoAnswer:
+				self._debug("mailexchange", mx, "has no AAAA record", level="!")
+			except dns.resolver.NXDOMAIN:
+				self._debug("NXDOMAIN", mx.exchange)
+
+		return mx_set
 	
 	def _http(self):
 		pass
@@ -44,5 +58,9 @@ class Check:
 		pass
 
 if __name__ == "__main__":
+	c = Check("ccc.de")
+	print(c._ns())
+	print(c._mx())
 	c = Check("heise.de")
-	c._ns()
+	print(c._ns())
+	print(c._mx())
