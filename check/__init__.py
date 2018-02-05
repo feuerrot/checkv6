96b339e615bdbe413de32fc4f9153b949f7a5f7e
Markus Witt <feuerrot@feuerrot.org>
2018-02-05 17:44:33 +0100

    check: slim down everything

--- check/__init__.py
+++ check/__init__.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 import dns.resolver
+import dns.name
 import requests
 
 class Check:
 	debug = True
 	
 	def __init__(self, hostname):
-		self.hostname = hostname
+		self.hostname = dns.name.from_text(hostname)
+		self.resolver = dns.resolver.Resolver()
+		#self.resolver.nameservers = ["::1", "127.0.0.1"]
+		self.result = {}
 	
 	def _debug(self, *args, level=None):
 		if self.debug:
@@ -14,53 +18,55 @@ class Check:
 				"[{}]".format(level or "x"),
 				" ".join([str(arg) for arg in args])
 			)
-	
-	def __filter_mx(self, mx):
-		return mx
 
-	def _query(self, type, hostname=None):
-		if type == dns.rdatatype.MX:
-			hostname = self.__filter_mx(hostname or self.hostname)
-		return dns.resolver.query(hostname or self.hostname, type)
+	def _query(self, record, hostname=None):
+		query = self.resolver.query(hostname or self.hostname, record)
+		return [answer for answer in query]
+	
+	def _get_first_record(self, record, hostname):
+		self._get_soa()
+		assert(self.soa.is_superdomain(hostname))
+
+		while self.soa.is_superdomain(hostname):
+			try:
+				result = self._query(record, hostname)
+				return result
+			except dns.resolver.NoAnswer:
+				hostname = hostname.parent()
+
+		return None
 	
 	def _aaaa(self, hostname=None):
-		query = self._query(dns.rdatatype.AAAA, hostname)
-		return [answer for answer in query.rrset]
+		return self._query(dns.rdatatype.AAAA, hostname)
 	
-	def _ns(self, hostname=None):
-		query = self._query(dns.rdatatype.NS)
-		ns_set = {}
-		for ns in query.rrset:
-			try:
-				ns_set[ns] = self._aaaa(ns.to_text())
-			except dns.resolver.NoAnswer:
-				self._debug("nameserver", ns, "has no AAAA record", level="!")
-
-		return ns_set
+	def _ns(self):
+		query = self._get_first_record(dns.rdatatype.NS, self.hostname)
+		return [answer.target for answer in query]
 	
 	def _mx(self):
-		query = self._query(dns.rdatatype.MX)
-		mx_set = {}
-		for mx in query.rrset:
-			try:
-				mx_set[mx] = self._aaaa(mx.exchange.to_text())
-			except dns.resolver.NoAnswer:
-				self._debug("mailexchange", mx, "has no AAAA record", level="!")
-			except dns.resolver.NXDOMAIN:
-				self._debug("NXDOMAIN", mx.exchange)
+		query = self._get_first_record(dns.rdatatype.MX, self.hostname)
+		return [answer.exchange for answer in query]
 
-		return mx_set
+	def _get_soa(self):
+		query = self.resolver.query(
+			self.hostname,
+			dns.rdatatype.SOA,
+			raise_on_no_answer=False
+		)
+		self.soa = query.response.authority[0].name
 	
 	def _http(self):
 		pass
 	
 	def _https(self):
 		pass
-
+	
+	def check(self):
+		self.result["ns"] = self._ns()
+		self.result["mx"] = self._mx()
+		self.result["aaaa"] = self._aaaa()
+		return self.result
+	
 if __name__ == "__main__":
-	c = Check("ccc.de")
-	print(c._ns())
-	print(c._mx())
-	c = Check("heise.de")
-	print(c._ns())
-	print(c._mx())
+	c = Check("www.chaosdorf.de")
+	print(c.check())
